676b24fd2edf31737b07d250d920eac36960d64b
deniyudi <deniyudi2@gmail.com>
2023-12-05 16:43:08 +0700

    SLEBEWWW

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
         <h1 className="text-4xl font-medium">Login</h1>
-        <p className="text-slate-500">Hi, Welcome back 👋</p>
+        <p className="text-slate-500">yOOO, Disini ada yanto Welcome back 👋</p>
 
         <div className="my-5">
             <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
